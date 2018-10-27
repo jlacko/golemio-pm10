620cb6dba496d74d756b7547f43fc8b0a4e2b504
Jindra Lacko <jindra.lacko@gmail.com>
2018-10-27 11:19:22 +0200

    nula na vstupu není nula, ale chyba

--- 1.1-get-data-stanice.R
+++ 1.1-get-data-stanice.R
@@ -7,8 +7,8 @@ source('0-get-token.R') # výsledek = objekt response s access tokenem v sobě
 
 odkaz <- paste0('https://ckc-emea.cisco.com/t/prague-city.com/cdp/v1/opendata/1.0/prague/',
                 '?domain=airqualityreports', # meteo stanice
-                '&fromDate=', Sys.Date()-1,
-                '&toDate=', Sys.Date(),
+                '&fromDate=', Sys.Date()-1, # od včerejška ...
+                '&toDate=', Sys.Date(), # ... do dneška
                 '&count=1000', # 17 čidel × 24 hodin × 2 pozorování do hodiny = 816
                 '&format=json')
 
@@ -20,6 +20,9 @@ stop_for_status(golemio)
 stanice_header <- c('identifier', 'name', 'source_timestamp', 'latitude', 'longitude', 'aqi', 'CO', 'SO2', 'PM10')
 
 data <- content(golemio)$data %>%
-  map_df(magrittr::extract, stanice_header)
+  map_df(magrittr::extract, stanice_header) 
+
+data[data == 0] <- NA # nula neznamená nulu, ale chybu
 
 write.csv2(data, './data/stanice.csv', row.names = F)
+
